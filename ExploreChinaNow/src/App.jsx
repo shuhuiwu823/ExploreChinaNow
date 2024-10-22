92a4d62d7f63695642e4e688461907305461fd58
Lucas4413 <shi.chenghao@northeastern.edu>
2024-10-21 22:16:36 -0400

    update the page layout

--- ExploreChinaNow/src/App.jsx
+++ ExploreChinaNow/src/App.jsx
@@ -1,20 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import MainBody from './MainBody.jsx';
+import Footer from './Footer.jsx';
+import Header from './Header.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <button
-    onClick={ () => {
-    fetch('/api/homepage');
-    }}
-    >Test</button>
+      <Header></Header>
+      <MainBody></MainBody>
+      <Footer></Footer>
     </>
-    )
+    );
 }
 
-export default App
+export default App;
---
+++ ExploreChinaNow/src/MainBody.jsx
@@ -0,0 +1,10 @@
+
+function MainBody() {
+    // Show Word component if loggedin, else show LogIn component 
+    return(
+        <main className="main">
+        </main>
+    );
+}
+
+export default MainBody;
