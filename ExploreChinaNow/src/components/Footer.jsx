fc565fe8eef3563745a151b290b2db9c55d10d0a
Jiale-Lyu <lyu.jial@northeastern.edu>
2024-11-10 16:17:01 -0500

    add Header and Footer, update icon

---
+++ ExploreChinaNow/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+// import React from "react";
+
+function Footer() {
+	return (
+		<footer style={{ padding: "20px", backgroundColor: "#333", color: "white", textAlign: "center" }}>
+			<p>&copy; 2024 Explore China Now. All rights reserved.</p>
+		</footer>
+	);
+}
+
+export default Footer;
